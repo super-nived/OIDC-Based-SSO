5bdcd5f5511175cb31dc35068cce413e82495cf9
super-nived <nivedchandran7@gmail.com>
2023-12-06 01:32:53 +0530

    second commmit

--- service_a/admin.py
+++ service_a/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+# service_a/admin.py
+from django.contrib import admin
+from .models import UserProfileA
+
+admin.site.register(UserProfileA)
--- service_b/admin.py
+++ service_b/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+# service_b/admin.py
+from django.contrib import admin
+from .models import UserProfileB
+
+admin.site.register(UserProfileB)
